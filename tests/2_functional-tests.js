2d83ba057685084f00b79f35187c7c75549746b8
Glitch (receptive-society) <none>
2020-01-13 14:00:59 +0000

    🎠🐝 Checkpoint ./tests/2_functional-tests.js:9419671/1411

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -44,17 +44,27 @@ suite('Functional Tests', function() {
         chai.request(server)
           .post('/api/books')
           .send({
-          title: 'Harry Poster'
-        })
+            title: 'Harry Poster'
+            })
           .end((err, res) => {
-          assert.equal(res.status, 200)
-          assert.equal(res.body.title, 'Harry Poster')
-          
+            assert.equal(res.status, 200)
+            assert.equal(res.body.title, 'Harry Poster')
+            assert.property(res.body, 'title', 'Books in array should contain title')
+            assert.property(res.body, '_id', 'Books in array should contain _id')
+            done()
         })
         //done();
       });
       
       test('Test POST /api/books with no title given', function(done) {
+        chai.request(server)
+          .post('/api/books')
+          .send({})
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.text, "Please enter a book title")
+            done()
+        })
         //done();
       });
       
@@ -64,7 +74,16 @@ suite('Functional Tests', function() {
     suite('GET /api/books => array of books', function(){
       
       test('Test GET /api/books',  function(done){
-        //done();
+        chai.request(server)
+          .get('/api/books')
+          .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.isArray(res.body, 'response should be an array');
+            assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
+            assert.property(res.body[0], 'title', 'Books in array should contain title');
+            assert.property(res.body[0], '_id', 'Books in array should contain _id');
+            done();
+          });
       });      
       
     });
@@ -73,11 +92,11 @@ suite('Functional Tests', function() {
     suite('GET /api/books/[id] => book object with [id]', function(){
       
       test('Test GET /api/books/[id] with id not in db',  function(done){
-        //done();
+        done();
       });
       
       test('Test GET /api/books/[id] with valid id in db',  function(done){
-        //done();
+        done();
       });
       
     });
@@ -86,7 +105,7 @@ suite('Functional Tests', function() {
     suite('POST /api/books/[id] => add comment/expect book object with id', function(){
       
       test('Test POST /api/books/[id] with comment', function(done){
-        //done();
+        done();
       });
       
     });
